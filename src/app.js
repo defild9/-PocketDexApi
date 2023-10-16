264ae094967b1f26a9e3cbf56c67d525408f9341
barab <ipz213_bvv@student.ztu>
2023-10-16 09:57:26 +0300

    Added routes for authentication, users and pokémon

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import authRoutes from './routes/authRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import pokemonsRoutes from './routes/pokemonsRoutes.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/auth', authRoutes)
+app.use('/user', userRoutes)
+app.use('/pokemons', pokemonsRoutes)
 
 app.listen(4444, (err) => {
   if (err) {
