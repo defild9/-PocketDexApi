1d2bcbcb5afabb3c656f769d585723aa86060040
Yevhenii Biletskyi <biletskijzena@gmail.com>
2023-10-18 17:07:04 +0300

    Made some changes in register validation

--- src/middlewares/validations/registerUserValidation.js
+++ src/middlewares/validations/registerUserValidation.js
@@ -1,7 +1,7 @@
 import { body } from 'express-validator'
 
 export const registerValidation = [
-  body('username', 'Minimal length for username is 5 characters').isLength({ min: 3 }),
+  body('username', 'Minimal length for username is 3 characters').isLength({ min: 3 }),
   body('email', 'Incorrect mail format').isEmail(),
   body('password', 'Password must be a minimum of 5 characters').isLength({ min: 5 }),
   body('createAt', 'Incorrect date format').optional().isDate(),
