5e8d4a58bc3b6720eb3f55787996ceb5ca821e61
Yevhenii Biletskyi <biletskijzena@gmail.com>
2023-10-18 17:08:27 +0300

    Created register and login method in controller and add route for login

--- src/controllers/authContoller.js
+++ src/controllers/authContoller.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import 'dotenv/config'
 
 import UserModel from '../models/User.js'
 
@@ -8,7 +9,79 @@ export const register = async (req, res) => {
     const password = req.body.password
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
-  } catch (error) {
+    const secretKey = process.env.SECRET_KEY
 
+    const doc = new UserModel({
+      username: req.body.username,
+      email: req.body.email,
+      passwordHash: hash
+    })
+
+    const user = await doc.save()
+
+    const token = jwt.sign(
+      {
+        _id: user._id
+      },
+      secretKey,
+      {
+        expiresIn: '30d'
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token
+    }
+    )
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Failed to register'
+    })
+  }
+}
+
+export const login = async (req, res) => {
+  const secretKey = process.env.SECRET_KEY
+  try {
+    const user = await UserModel.findOne({ email: req.body.email })
+    if (!user) {
+      return req.status(404).json({
+        message: 'User not found'
+      })
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+    if (!isValidPass) {
+      return req.status(400).json({
+        message: 'Invalid login or password'
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id
+      },
+      secretKey,
+      {
+        expiresIn: '30d'
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token
+    }
+    )
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Failed to authorise'
+    })
   }
 }
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import { registerValidation } from '../middlewares/validations/registerUserValidation.js'
+import handleValidationErrors from '../middlewares/handleValidationErrors.js'
+import { login, register } from '../controllers/authContoller.js'
+import { loginValidation } from '../middlewares/validations/loginUserValidation.js'
 
 const router = express.Router()
 
-router.post('/register')
+router.post('/register', registerValidation, handleValidationErrors, register)
+
+router.post('/login', loginValidation, handleValidationErrors, login)
 
 export default router
