bb583088aebe664964fc3d2a5e0db71524fdad77
Yevhenii Biletskyi <biletskijzena@gmail.com>
2023-11-11 12:59:55 +0200

    Added cors

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ import pokemonsRoutes from './src/routes/pokemonsRoutes.js'
 import passport from './src/auth/passportConfig.js'
 import dotenv from 'dotenv'
 import './src/data/database.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(passport.initialize())
-
 app.use(express.json())
 
 app.use('/auth', authRoutes)
